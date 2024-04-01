65762d043f56f7c46c70f1935ea42fec7e5d07af
Dmitriy <dmitry.mishin@buildsafe.se>
2024-04-01 07:53:17 +0400

    move method to data source

---
+++ app/src/main/java/mishin/trader/net/test/data/datasource/TickersLocalDataSource.kt
@@ -0,0 +1,14 @@
+package mishin.trader.net.test.data.datasource
+
+import android.util.Log
+import mishin.trader.net.test.domain.Ticker
+
+class TickersLocalDataSource {
+    fun getTickers(): List<Ticker> {
+        Log.wtf("Tickers", "get local")
+        return "SP500.IDX,AAPL.US,RSTI,GAZP,MRKZ,RUAL,HYDR,MRKS,SBER,FEES,TGKA,VTBR,AN H.US,VICL.US,BURG.US,NBL.US,YETI.US,WSFS.US,NIO.US,DXC.US,MIC.US,HSBC.US,EXPN.EU,GSK.EU,SHP.EU,MAN.EU,DB1.EU,MUV2.EU,TATE.EU,KGF.EU,MGGT.EU,SG GD.EU"
+            .split(",")
+            .toList()
+            .map { Ticker(it) }
+    }
+}
--- app/src/main/java/mishin/trader/net/test/data/repository/TicketsRepositoryImpl.kt
+++ app/src/main/java/mishin/trader/net/test/data/repository/TicketsRepositoryImpl.kt
@@ -7,22 +7,17 @@ import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import mishin.trader.net.test.data.datasource.TickersLocalDataSource
 import mishin.trader.net.test.data.network.rest.ApiParams
 import mishin.trader.net.test.data.network.rest.entity.TickersResponse
 import mishin.trader.net.test.domain.Ticker
 import mishin.trader.net.test.domain.TickersRepository
 
 class TicketsRepositoryImpl(
+    private val localDataSource: TickersLocalDataSource,
     private val httpClient: HttpClient,
     private val json: Json
 ) : TickersRepository {
-    private fun getLocalTickers(): List<Ticker> {
-        Log.wtf("Tickers", "get local")
-        return "SP500.IDX,AAPL.US,RSTI,GAZP,MRKZ,RUAL,HYDR,MRKS,SBER,FEES,TGKA,VTBR,AN H.US,VICL.US,BURG.US,NBL.US,YETI.US,WSFS.US,NIO.US,DXC.US,MIC.US,HSBC.US,EXPN.EU,GSK.EU,SHP.EU,MAN.EU,DB1.EU,MUV2.EU,TATE.EU,KGF.EU,MGGT.EU,SG GD.EU"
-            .split(",")
-            .toList()
-            .map { Ticker(it) }
-    }
 
     override suspend fun getTickers(): List<Ticker> {
         Log.wtf("Tickers", "request tickets")
@@ -46,6 +41,6 @@ class TicketsRepositoryImpl(
                 Log.wtf("Tickers", "exception $e")
             }
         }
-        return getLocalTickers()
+        return localDataSource.getTickers()
     }
 }
