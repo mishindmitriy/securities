4ff3b9f63fe60236c2ad236018ad1eab35e68eba
Dmitriy <dmitry.mishin@buildsafe.se>
2024-04-01 17:12:58 +0400

    upd doc

--- app/src/main/java/mishin/trader/net/test/data/network/quotations/QuotationRawData.kt
+++ app/src/main/java/mishin/trader/net/test/data/network/quotations/QuotationRawData.kt
@@ -2,27 +2,31 @@ package mishin.trader.net.test.data.network.quotations
 
 import kotlinx.serialization.SerialName
 
+/**
+ * @param ticker - Тикер
+ * @param changePercent - Изменение в процентах относительно цены закрытия предыдущей торговой
+ * сессии
+ * @param lastTradeExchange - Биржа последней сделки
+ * @param name - Название бумаги
+ * @param lastTradePrice - Цена последней сделки
+ * @param change - Изменение цены последней сделки в пунктах относительно цены закрытия предыдущей
+ * торговой сессии
+ * @param minStep - Шаг округления
+ */
 @kotlinx.serialization.Serializable
 data class QuotationRawData(
     @SerialName("c")
     var ticker: String? = null,
-    @SerialName("name")
-    var name: String? = null,
-
-    //Изменение в процентах относительно цены закрытия предыдущей торговой сессии
     @SerialName("pcp")
     var changePercent: Double? = null,
-
     @SerialName("ltr")
-    var lastTradeExchange: String? = null, //Биржа последней сделки
-
+    var lastTradeExchange: String? = null,
+    @SerialName("name")
+    var name: String? = null,
     @SerialName("ltp")
-    var lastTradePrice: Double? = null, //Цена последней сделки
-
-    //Изменение цены последней сделки в пунктах относительно цены закрытия предыдущей торговой сессии
+    var lastTradePrice: Double? = null,
     @SerialName("chg")
     var change: Double? = null,
-
     @SerialName("min_step")
     var minStep: Double? = null
 )
